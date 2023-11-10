5a7c818e409f85f81c2fcbdec1760bb99b4f4342
fnando-gomez <fnando.gomez@gmail.com>
2023-11-09 22:35:12 -0600

    Back to server

--- server0.js
+++ server0.js
@@ -1,33 +1,25 @@
-import express from 'express';
-import path from 'path';
-import vhost from 'vhost';
-import fs from 'fs';
-const port = 80;
+const express = require('express');
+const path = require('path');
 
 
 const app = express();
-
-const routerMain = express.Router();
-const appTunnel = express.Router();
+const port = 80;
 
 // Define a route to serve the HTML page
-routerMain.get('/', (req, res) => {
-    const htmlPath = path.join('./index.html');
+app.get('/', (req, res) => {
+    const htmlPath = path.join(__dirname, 'index.html');
     res.sendFile(htmlPath);
 });
 
-appTunnel.get('/', (req, res) => {
-    const htmlPath = path.join('./indexTunnel.html');
-    res.sendFile(htmlPath);
-});
 
-routerMain.get('/headers', (req, res) => {
-    const htmlPath = path.join('./headers.html');
+app.get('/headers', (req, res) => {
+    const htmlPath = path.join(__dirname, 'headers.html');
     
     // Get all HTTP request headers
     const headers = JSON.stringify(req.headers, null, 2);
 
     // Read the HTML file and replace a placeholder with the headers
+    const fs = require('fs');
     fs.readFile(htmlPath, 'utf8', (err, data) => {
         if (err) {
             return res.status(500).send('Error reading HTML file');
@@ -36,9 +28,6 @@ routerMain.get('/headers', (req, res) => {
         res.send(data);
     });
 });
-
-app.use(vhost('hawkingslab.online', routerMain));
-app.use(vhost('tunnel.hawkingslab.online', appTunnel));
 
 // Start the server
 app.listen(port, () => {
